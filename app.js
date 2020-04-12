c3f3e20e0cb99a6b9ed9aca12188e014d50639c0
Nazim Uddin <umnazim@gmail.com>
2020-04-12 00:08:01 -0700

    api get request updated

--- app.js
+++ app.js
@@ -1,9 +1,17 @@
 const express = require("express");
 
+const https = require("https");
+
 const app = express();
 
 app.get("/", function (req, res) {
-  res.send("Server is up and running.");
+  const url =
+    "https://api.openweathermap.org/data/2.5/weather?q=%20San%20Francisco&appid=7c495b0d43dec3868257874c48b758c4&units=metric";
+
+  https.get(url, function (response) {
+    console.log(response);
+  });
+   res.send("Server is up and running.");
 });
 
 app.listen(3000, function () {
